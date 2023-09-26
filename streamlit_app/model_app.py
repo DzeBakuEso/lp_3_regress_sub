5d6927d06d28f2cef820ef95e5c463b9ba8d99c3
ZEUS <137338216+DzeBakuEso@users.noreply.github.com>
2023-09-26 04:57:46 -1200

    updates

--- streamlit_app/model_app.py
+++ streamlit_app/model_app.py
@@ -1,54 +1,101 @@
-# below we are importing the necessary packages 
-import streamlit as st 
+import streamlit as st
 import pickle
-import numpy as np 
-import pandas as pd 
+import joblib
+import numpy as np
+import pandas as pd
 import os
 from xgboost import XGBRegressor
 
-# below we defined a function to load the train data set 
-def train_load(relative_path):
-    train_data = pd.read_csv(relative_path)
-    return train_data  # You should return the loaded data, not the function itself
+# here we are setting the canvas (background) color to light blue
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: lightblue;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-# below we defined a function to locate and return the path of our train set
+# Below we defined the title with a red color using Markdown
+st.markdown("<h1 style='color: lightgoldenrodyellow;'>Store Sales Regression Model App</h1>", unsafe_allow_html=True)
+
+# below we defined the app sections
+Header = st.container()
+Prediction_section = st.container()
+
+#the function here is to locate and return the path of our train set
 def find_train_set(file_name, search_directory):
     for root, dirs, files in os.walk(search_directory):
         if file_name in files:
-            return os.path.join(root, file_name)  # Corrected the os.path.join method
+            return os.path.join(root, file_name)  
     return None
 
-# below we are declaring variables to store both the file and the location to search
+#below here we defined a function to load our train data
+@st.cache_data
+def train_load(relative_path):
+    train_data = pd.read_csv(relative_path)
+    return train_data
+
+# below we declared variables to store both the file and the location to search
 file_to_find = 'X_train.csv'
 path_to_search = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress'
 
 file_path = find_train_set(file_to_find, path_to_search)
 
-# below we are calling the function to load the data set  
+#below here we call the function to load the data set
 train_data_df = train_load(file_path)
 
-# below are setting the title of the app 
-st.title('Store Sales Regression Model App')
-
-# below is the main content area 
-Header = st.container()
-Data_section  = st.container()
-Prediction_section = st.container()
-
-# below is is the app description and introduction
+#the code below is to describe the app and provide an introduction
 with Header:
-    Header.write('Welcome to the Store Sales Regression Model App!')
-    Header.write('This app uses machine learning to predict store sales based on historical data.')
-    Header.write('To use the app, follow these steps:')
-    Header.markdown('- Please enter your inputs values')
-    Header.markdown('- Adjust any necessary settings or parameters.')
-    Header.markdown('- Click the "Predict" button to generate sales predictions.')
+    st.write('Welcome to the Store Sales Regression Model App!')
+    st.write('This app uses machine learning to predict store sales based on historical data.')
+    st.write('To use the app, follow these steps:')
+    st.markdown('- Please enter your inputs values', unsafe_allow_html=True)
+    st.markdown('- Adjust any necessary settings or parameters', unsafe_allow_html=True)
+    st.markdown('- Click the "Predict" button to generate sales predictions', unsafe_allow_html=True)
 
-# below we are setting and loading our data set 
+# below here we load the data set and preview it
 with st.expander('Data Preview'):
     st.write('**Training Data Preview:**')
-    st.write(train_data_df.head()) # allowing the user to preview the loaded train data
-    st.write(f'Model Training  Data Shape: {train_data_df.shape}')
+    st.write(train_data_df.head())  # Allowing the user to preview the loaded train data
+    st.write(f'Model Training Data Shape: {train_data_df.shape}')
 
+#below here we defined a function to load the preprocessing tools
+def load_preprocessing_tools(tool_path):
+    with open(tool_path, 'rb') as file:
+        loaded_tools = pickle.load(file)
+    return loaded_tools
+
+#here we specified the full file paths to the tool files
+encoder_path = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/family_encoding.pkl'
+scaler_path = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/standard_scaler.pkl'
+
+#below here we Load the preprocessing tools
+loaded_encoder = load_preprocessing_tools(encoder_path)
+loaded_scaler = load_preprocessing_tools(scaler_path)
+
+#Below we want to import our model
+
+def find_model(model_name, model_directory):
+    for root, dirs, files in os.walk(model_directory): # here we defined a function to look for the path of the model
+        if model_name in files:
+            return os.path.join(root, model_name)
+    return None
+
+model_name = 'xgboost_model.pkl'
+model_directory = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/'
+
+model_path = find_model(model_name, model_directory ) # here we call the function
+
+# below is a function to load the our model using the path from the above model finding code 
+
+def load_xgboost_model(model_path):
+    with open(model_path, 'rb') as file:
+        xgboost_model = pickle.load(file)
+    return xgboost_model
+
+loaded_xgboost_model = load_xgboost_model(model_path) # here we call and pass in the path as argument to load our model 
 
 
