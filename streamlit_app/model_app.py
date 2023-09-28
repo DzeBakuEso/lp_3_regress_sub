d44ba3a0299d2efd2b3e8f26e5b0791485709bc3
ZEUS <137338216+DzeBakuEso@users.noreply.github.com>
2023-09-28 10:13:35 -1200

    updates

--- streamlit_app/model_app.py
+++ streamlit_app/model_app.py
@@ -1,12 +1,13 @@
+# belowe we import our packages 
+
 import streamlit as st
 import pickle
-import joblib
 import numpy as np
 import pandas as pd
 import os
 from xgboost import XGBRegressor
 
-# here we are setting the canvas (background) color to light blue
+# Setting the canvas (background) color to light blue
 st.markdown(
     """
     <style>
@@ -18,84 +19,92 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Below we defined the title with a red color using Markdown
-st.markdown("<h1 style='color: lightgoldenrodyellow;'>Store Sales Regression Model App</h1>", unsafe_allow_html=True)
+# Define the welcome message and instructions as a multiline string
+welcome_message = """
+# Store Sales Regression Model App
 
-# below we defined the app sections
-Header = st.container()
-Prediction_section = st.container()
+This app uses machine learning to predict store sales based on historical data.
 
-#the function here is to locate and return the path of our train set
+To use the app, follow these steps:
+
+- Please enter your input values.
+- Adjust any necessary settings or parameters.
+- Click the "Predict" button to generate sales predictions.
+"""
+
+# Below we defined the app sections
+header = st.container()
+prediction_section = st.container()
+
+#below is a unction to locate and return the path of the train set
 def find_train_set(file_name, search_directory):
     for root, dirs, files in os.walk(search_directory):
         if file_name in files:
-            return os.path.join(root, file_name)  
+            return os.path.join(root, file_name)
     return None
 
-#below here we defined a function to load our train data
+#below we defined function to load train data
 @st.cache_data
 def train_load(relative_path):
     train_data = pd.read_csv(relative_path)
     return train_data
 
-# below we declared variables to store both the file and the location to search
+#below we declared variables to store both the file and the location to search
 file_to_find = 'X_train.csv'
-path_to_search = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress'
+path_to_search = os.path.join(os.getcwd(), 'data')
 
 file_path = find_train_set(file_to_find, path_to_search)
 
-#below here we call the function to load the data set
+# Load the data set
 train_data_df = train_load(file_path)
+#below we create a dropdown to toggle visibility of the training data
+show_train_data = st.checkbox("Show Training Data")
 
-#the code below is to describe the app and provide an introduction
-with Header:
-    st.write('Welcome to the Store Sales Regression Model App!')
-    st.write('This app uses machine learning to predict store sales based on historical data.')
-    st.write('To use the app, follow these steps:')
-    st.markdown('- Please enter your inputs values', unsafe_allow_html=True)
-    st.markdown('- Adjust any necessary settings or parameters', unsafe_allow_html=True)
-    st.markdown('- Click the "Predict" button to generate sales predictions', unsafe_allow_html=True)
-
-# below here we load the data set and preview it
-with st.expander('Data Preview'):
+#below this code displayed the loaded data if the checkbox is selected
+if show_train_data:
     st.write('**Training Data Preview:**')
-    st.write(train_data_df.head())  # Allowing the user to preview the loaded train data
+    st.write(train_data_df.head())  # Allow the user to preview the loaded train data
     st.write(f'Model Training Data Shape: {train_data_df.shape}')
 
-#below here we defined a function to load the preprocessing tools
+#below is a function to load the preprocessing tools
 def load_preprocessing_tools(tool_path):
     with open(tool_path, 'rb') as file:
         loaded_tools = pickle.load(file)
     return loaded_tools
 
-#here we specified the full file paths to the tool files
-encoder_path = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/family_encoding.pkl'
-scaler_path = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/standard_scaler.pkl'
+#below we Defined relative paths for the preprocessing tools
+data_directory = os.path.join(os.getcwd(), 'data')
+encoder_path = os.path.join(data_directory, 'family_encoding.pkl')
+scaler_path = os.path.join(data_directory, 'standard_scaler.pkl')
 
-#below here we Load the preprocessing tools
+#below we Load the preprocessing tools
 loaded_encoder = load_preprocessing_tools(encoder_path)
 loaded_scaler = load_preprocessing_tools(scaler_path)
 
-#Below we want to import our model
-
+#below we defined a function to locate and return the path of the model
 def find_model(model_name, model_directory):
-    for root, dirs, files in os.walk(model_directory): # here we defined a function to look for the path of the model
+    for root, dirs, files in os.walk(model_directory):
         if model_name in files:
             return os.path.join(root, model_name)
     return None
 
+#below we decalare a variables for model name and the directory
 model_name = 'xgboost_model.pkl'
-model_directory = 'G:/AZUBI-AFRICA/CAREER_ACCELERATOR_ALL_OUT/LP3_REGR/lp3_submission_regress/lp_3_regress_sub/'
+model_directory = os.path.join(os.getcwd(), 'data')
 
-model_path = find_model(model_name, model_directory ) # here we call the function
+model_path = find_model(model_name, model_directory)
 
-# below is a function to load the our model using the path from the above model finding code 
-
+#below is a function to load the model
 def load_xgboost_model(model_path):
     with open(model_path, 'rb') as file:
         xgboost_model = pickle.load(file)
     return xgboost_model
 
-loaded_xgboost_model = load_xgboost_model(model_path) # here we call and pass in the path as argument to load our model 
+# here we are Loading the XGBoost model
+loaded_xgboost_model = load_xgboost_model(model_path)
+
+# Displaying the welcome message and instructions
+with header:
+    st.markdown(welcome_message, unsafe_allow_html=True)
 
 
